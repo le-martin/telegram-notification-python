be7ad7a4bc0d9884c23d57dcb2edd0ef1cb08120
Martin Le <martin.le@tu-braunschweig.de>
2024-03-30 04:31:30 +0100

    Add message splitting feature for messages with more than 4096 characters.

--- telegram.py
+++ telegram.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import configparser
+import math
 import socket
 import sys
 import time
@@ -15,9 +16,15 @@ def send_message(msg: str) -> None:
     CHAT_ID: str = config['DEFAULT']['CHAT_ID']
     HOSTNAME: str= socket.gethostname()
     now: str = time.strftime("%I:%M%p on %B %d, %Y")
-    new_msg: str = f"{HOSTNAME}({now}): \n {msg}"
-    url: str = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={new_msg}"
-    print(requests.get(url).json())  # Send message and print request output
+    new_msg: str = f"{HOSTNAME}({now}):\n{msg}"
+    num_msg: int = math.ceil(len(new_msg) / 4096)
+    for i in range(num_msg):
+        if i != num_msg - 1:
+            split_msg = new_msg[4096 * i:4096 * (i + 1)]
+        else:
+            split_msg = new_msg[4096 * i:]
+        url: str = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={split_msg}"
+        print(requests.get(url).json())  # Send message and print request output
 
 
 if __name__ == '__main__':
